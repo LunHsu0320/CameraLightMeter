9365b7bbabb37a80cdacc8c039bcc001dbcb1b22
Lun Hsu <text0320chatgpt@gnail.com>
2023-06-16 00:05:18 +0800

    新增ISO光圈快門layout

--- app/src/main/java/com/example/cameralightmeter/MainActivity.kt
+++ app/src/main/java/com/example/cameralightmeter/MainActivity.kt
@@ -6,6 +6,10 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,6 +19,13 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private var lightSensor: Sensor? = null
     private lateinit var valueTextView: TextView
     private lateinit var supportStatusTextView: TextView
+    private lateinit var isoTextView: TextView
+    private lateinit var apertureTextView: TextView
+    private lateinit var shutterTextView: TextView
+    private lateinit var isoSpinner: Spinner
+    private lateinit var apertureSpinner: Spinner
+    private lateinit var shutterSpinner: Spinner
+
 
     private var lightSensorValue: Float = 0f
     private var hasReceivedSensorValue: Boolean = false
@@ -27,12 +38,75 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
         valueTextView = findViewById(R.id.light_value_textview)
         supportStatusTextView = findViewById(R.id.supportStatusTextView)
+        isoTextView = findViewById(R.id.isoTextView)
+        apertureTextView = findViewById(R.id.apertureTextView)
+        shutterTextView = findViewById(R.id.shutterTextView)
+        // 找到相應的 Spinner
+        isoSpinner = findViewById(R.id.isoSpinner)
+        apertureSpinner = findViewById(R.id.apertureSpinner)
+        shutterSpinner = findViewById(R.id.shutterSpinner)
 
         if (lightSensor == null) {
             supportStatusTextView.text = "設備不支援光感應器"
         } else {
             supportStatusTextView.text = "支援光感應器"
         }
+
+        // 設置 ISO、光圈和快門的數據源
+        val isoValues = arrayOf("100", "200", "400", "800", "1600")
+        val apertureValues = arrayOf("1.4", "2.0", "2.8", "4.0", "5.6")
+        val shutterValues = arrayOf("1/1000", "1/500", "1/250", "1/125", "1/60")
+
+        // 創建 ArrayAdapter 並設置數據源
+        val isoAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, isoValues)
+        isoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        isoSpinner.adapter = isoAdapter
+
+        val apertureAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, apertureValues)
+        apertureAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        apertureSpinner.adapter = apertureAdapter
+
+        val shutterAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, shutterValues)
+        shutterAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        shutterSpinner.adapter = shutterAdapter
+
+        // ISO Spinner 選擇監聽器
+        isoSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedISO = isoValues[position]
+                isoTextView.text = "ISO：$selectedISO"
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // 不執行任何操作
+            }
+        }
+
+        // 光圈 Spinner 選擇監聽器
+        apertureSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedAperture = apertureValues[position]
+                apertureTextView.text = "光圈：$selectedAperture"
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // 不執行任何操作
+            }
+        }
+
+        // 快門 Spinner 選擇監聽器
+        shutterSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedShutter = shutterValues[position]
+                shutterTextView.text = "快門：$selectedShutter"
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // 不執行任何操作
+            }
+        }
+
+
     }
 
     override fun onResume() {
@@ -61,7 +135,12 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 lightSensorValue = sensorEvent.values[0]
                 hasReceivedSensorValue = true
 
-                valueTextView.text = "光感應器數值：$lightSensorValue"
+                valueTextView.text = "光感應器數值：$lightSensorValue lux"
+//                isoTextView.text = "ISO: $isoValue"
+//                apertureTextView.text = "光圈: $apertureValue"
+//                shutterTextView.text = "快門: $shutterValue"
+
+
             }
         }
     }
